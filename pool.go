c7fc04ec98b9b233c343b837a19a6e63f4a02b9b
Bluek404 <i@bluek404.com>
2015-01-21 01:13:35 +0800

    修改等待方式

--- pool.go
+++ pool.go
@@ -1,35 +1,68 @@
 package pool
 
-func NewPool(maxThread int, f func()) *Pool {
-	return &Pool{
+import (
+	"errors"
+)
+
+var (
+	AlRunErr = errors.New("already running")
+)
+
+func NewPool(maxThread int, f func()) Pool {
+	return Pool{
 		maxThread: maxThread,
 		done:      make(chan bool, maxThread),
-		exit:      make(chan bool),
+		allDone:   make(chan bool),
 		f:         f,
 	}
 }
 
 type Pool struct {
 	maxThread int
+	threadNum int
 	f         func()
+	stop      bool
+	lock      bool
 	done      chan bool
-	exit      chan bool
+	allDone   chan bool
 }
 
-func (p *Pool) Run() {
-	go func() {
-		for i := 0; i < p.maxThread; i++ {
-			go p.run()
-		}
-		for {
-			select {
-			case <-p.done:
-				go p.run()
-			case <-p.exit:
-				return
+func (p *Pool) Run() error {
+	if !p.lock {
+		p.lock = true
+		p.stop = false
+		go func() {
+			// 开启预定数量线程
+			for i := 0; i < p.maxThread; i++ {
+				// 处理当预定数量线程还未开启完毕时
+				// 接受到的Stop信号
+				if !p.stop {
+					p.threadNum++
+					go p.run()
+				} else {
+					break
+				}
 			}
-		}
-	}()
+
+			// 保持线程数量
+			for {
+				<-p.done
+				if !p.stop {
+					go p.run()
+				} else {
+					p.threadNum--
+					if p.threadNum == 0 {
+						p.allDone <- true
+						p.lock = false
+					}
+				}
+			}
+		}()
+
+		return nil
+	}
+
+	return AlRunErr
 }
 
 func (p *Pool) run() {
@@ -37,10 +70,14 @@ func (p *Pool) run() {
 	p.done <- true
 }
 
+// 获取设置的最大线程数
 func (p *Pool) GetMaxThread() int {
 	return p.maxThread
 }
 
-func (p *Pool) Exit() {
-	p.exit <- true
+// 停止继续补充线程
+// 并等待已经开启的线程全部执行完毕
+func (p *Pool) Stop() {
+	p.stop = true
+	<-p.allDone
 }
